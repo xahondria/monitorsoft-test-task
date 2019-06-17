b22a33e12efb6633f230f3442c3243a4dac0363e
Vladimir Kolomoyets <kolomoyetsva@gmail.com>
2019-06-17 15:18:48 +0300

    added popup messages

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -3,12 +3,26 @@ import { Injectable } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class MessageService {
   messages: string[] = [];
+  popupMessages: string[] = [];
 
   add(message: string) {
     this.messages.unshift(message);
+    this.addPopupMessage(message);
+  }
+
+  addPopupMessage(message) {
+    this.popupMessages.unshift(message);
+    setTimeout(() => {
+      this.popupMessages.pop();
+    }, 5000);
+  }
+
+  removePopupMessage(index) {
+    this.popupMessages.splice(index, 1);
   }
 
   clear() {
     this.messages = [];
+    this.popupMessages = [];
   }
 }
